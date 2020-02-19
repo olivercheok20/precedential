0d4f87893868bcd2288fd4b84bb86c66b2f4d63a
olivercheok20 <olivercheok20@gmail.com>
2020-02-19 23:18:57 +0800

    Enable retrieval of documents from firebase

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 require("firebase/firestore");
 
 firebase.initializeApp(firebaseConfig.config);
+const firestore = firebase.firestore();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,12 +34,57 @@ app.get('/resources', (req, res) => {
 });
 
 app.get('/research', (req, res) => {
-    console.log(req.headers.charge);
-    console.log(req.headers.type);
-    console.log(req.headers.quantity);
-    console.log(req.headers.keywords);
+    const charge = req.headers.charge;
+    const type = req.headers.type;
+    const quantity = req.headers.quantity;
+    const keywords = req.headers.keywords;
+
+    let statutesViolated = '';
+    let prescribedSentence = '';
+    let rangeOfSentences = '';
+    let similarCases = '';
+    let sentencingEstimate = '';
+
+    let caseRef = firestore.collection('research');
+    let query = caseRef.get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                return;
+            }
+
+            snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data());
+            })
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);
+        })
+
+    switch (charge) {
+        case "Import and export of controlled drugs":
+            statutesViolated = '7 Import and export of controlled drugs';
+            break;
+        case "Trafficking in controlled drugs":
+            statutesViolated = '5 Trafficking in controlled drugs';
+            break;
+        case "Possession of controlled drugs":
+            statutesViolated = '8 Possession of controlled drugs';
+            break;
+        case "Consumption of controlled drugs":
+            statutesViolated = '8 Consumption of controlled drugs';
+            break;
+        case "Arranging or planning gatherings where controlled drugs are to be consumed or trafficked":
+            statutesViolated = '11A Arranging or planning gatherings where controlled drugs are to be consumed or trafficked';
+            break;
+        case "Possession of pipes, utensils, etc.":
+            statutesViolated = '9 Possession of pipes, utensils, etc.';
+            break;
+
+    }
+
     res.send({
-        statutesViolated: '1',
+        statutesViolated: statutesViolated,
         prescribedSentence: '2',
         rangeOfSentences: '3',
         similarCases: '4',
